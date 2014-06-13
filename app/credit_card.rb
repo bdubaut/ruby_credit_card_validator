48f987443cd60503e3efff07a9c4e682bec990fd
Bertrand Dubaut <b.dubaut@gmail.com>
2014-06-13 09:04:58 +0200

    Check Luhn OK

--- app/credit_card.rb
+++ app/credit_card.rb
@@ -1,3 +1,16 @@
 class CreditCard
+	attr_accessor :ccnumber
 
+	def check_luhn
+	  s1 = 0
+	  s2 = 0
+	  self.ccnumber.to_s.reverse.chars.each_slice(2) do |odd, even|
+	    s1 += odd.to_i
+
+	    double = even.to_i * 2
+	    double -= 9 if double >= 10
+	    s2 += double
+	  end
+	  (s1 + s2) % 10 == 0
+	end
 end
